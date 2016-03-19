c79628b15853722094159d917f069f75af6e533d
Valery Bugakov <skymk1@gmail.com>
2016-03-19 11:33:17 +0400

    simplify updater

--- README.md
+++ README.md
@@ -4,13 +4,49 @@ Usage
 Updater machine
 ------------
 
-Add CNAME to updater_config.json `{ host: CNAME }`
-Add CNAME to hosts file if necessary.
+Change HOSTNAME in config.json `{ host: "HOSTNAME" }`
+Change deviceId in config.json `{ deviceId: "TestMachine" }` that id will be used to
+identify machine when sending updates.
+Copy certs folder from server machine.
 
-```
+```sh
 git clone https://github.com/valerybugakov/lamassu-updater.git
+cd lamassu-updater
 
 scp -r user@host:/path/to/generated/cetrs certs
 npm start
 ```
 
+Add upstart job (for new machines)
+
+Create file:
+
+```sh
+sudo nano /etc/init/rce-client.conf
+```
+
+Copy paste:
+
+```
+description "rce-client"
+start on runlevel [2345]
+stop on runlevel [!2345]
+respawn
+respawn limit 3 60
+script
+        chdir /root/rce-client/
+        exec node updater
+end script
+```
+
+Start:
+
+```sh
+start rce-client
+```
+
+Debug
+
+```sh
+tail -f /var/log/upstart/rce-client.log
+```
--- updater.js
+++ updater.js
@@ -1,157 +1,84 @@
 var https = require('https')
 var fs = require('fs')
-var EventEmitter = require('events').EventEmitter
-var util = require('util')
 var path = require('path')
 var exec = require('child_process').exec
 
-process.on('SIGUSR2', function () { process.exit() })
-process.on('SIGTERM', function () { /* Immune */ })
+var downloading;
+var config = JSON.parse(fs.readFileSync(path.resolve(__dirname, './config.json')))
+var packagePath = config.downloadDir + '/update.sh'
 
-var UPDATER_CONFIG = path.resolve(__dirname, './updater_config.json')
-var config = JSON.parse(fs.readFileSync(UPDATER_CONFIG))
-
-var Updater = function (config) {
-  this.config = config
-}
-
-util.inherits(Updater, EventEmitter)
-
-Updater.factory = function factory (config) {
-  return new Updater(config)
-}
-
-Updater.prototype.run = function run () {
-  if (!this.init()) {
-    console.log('Certificate files not available yet, exiting.')
-    return
+var httpsOptions = {
+  host: config.host,
+  port: config.port,
+  key: fs.readFileSync(config.certs.keyFile),
+  cert: fs.readFileSync(config.certs.certFile),
+  ca: fs.readFileSync(config.certs.caFile),
+  ciphers: 'AES128-GCM-SHA256:RC4:HIGH:!MD5:!aNULL:!EDH',
+  secureProtocol: 'TLSv1_method',
+  rejectUnauthorized: true,
+  headers: {
+    'device-id': config.deviceId
   }
-
-  this.update()
-  var self = this
-
-  setInterval(function () { self.update() }, this.config.updateInterval)
-  // setInterval(function () { self.die() }, this.config.deathInterval)
 }
 
-function fetchVersion () {
-  var str = fs.readFileSync('./package.json')
-  var packageJson = JSON.parse(str)
-  return packageJson.version
-}
+console.log('Updater started as: ' + config.deviceId);
+update();
+setInterval(update, config.updateInterval);
 
-Updater.prototype.init = function init () {
-  var certs = {
-    certFile: this.config.certs.certFile,
-    keyFile: this.config.certs.keyFile,
-    caFile: this.config.certs.caFile
-  }
-
-  if (!fs.existsSync(certs.keyFile) || !fs.existsSync(certs.certFile)) {
-    return false
-  }
-
-  this.key = fs.readFileSync(certs.keyFile)
-  this.cert = fs.readFileSync(certs.certFile)
-  this.ca = fs.readFileSync(certs.caFile)
-
-  this.downloadDir = this.config.downloadDir
-  this.packagePath = this.config.downloadDir + '/update.sh'
-
-  this.version = fetchVersion()
-  this.httpsOptions = this.getHttpsOptions()
-  return true
-}
-
-Updater.prototype.cleanup = function () {
-  if (fs.existsSync(this.downloadDir)) {
-    if (fs.existsSync(this.packagePath)) fs.unlinkSync(this.packagePath)
+function cleanup() {
+  if (fs.existsSync(config.downloadDir)) {
+    if (fs.existsSync(packagePath)) {fs.unlinkSync(packagePath)}
   } else {
-    fs.mkdirSync(this.downloadDir)
+    fs.mkdirSync(config.downloadDir)
   }
 }
 
-Updater.prototype.getHttpsOptions = function getHttpsOptions () {
-  var config = this.config
-
-  var options = {
-    host: config.host,
-    port: config.port,
-    key: this.key,
-    cert: this.cert,
-    ca: this.ca,
-    ciphers: 'AES128-GCM-SHA256:RC4:HIGH:!MD5:!aNULL:!EDH',
-    secureProtocol: 'TLSv1_method',
-    rejectUnauthorized: true,
-    headers: {
-      'application-version': this.version,
-      'device-id': config.deviceId
-    }
-  }
-
-  options.agent = new https.Agent(options)
-  return options
-}
-
-Updater.prototype.update = function () {
-  if (this.downloading) return
-  this.cleanup()
-  var self = this
-
-  // console.log('\nMaking request...')
-  https.get(this.httpsOptions, function (res) {
+function update() {
+  cleanup()
+  if (downloading) {return;}
+  downloading = true;
+  https.get(httpsOptions, function (res) {
     var code = res.statusCode
-    // console.log('\nStatus Code: ', code);
-
     switch (code) {
-      case 304:
-        res.resume()
-        break
-      case 412:
-        res.resume()
-        self.emit('error', new Error('Server has lower version!'))
-        break
       case 200:
-        self.downloadFile(res)
-        break
+        downloadFile(res)
+        break;
       default:
         res.resume()
-        this.emit('error', new Error('Unknown response code: ' + code))
     }
-  }).on('error', noop)
+  }).on('error', function () {downloading = false;});
 }
 
-function noop () {}
-
-Updater.prototype.downloadFile = function (res) {
-  this.downloading = true
-  var self = this
-  var filename = path.join(__dirname, this.packagePath)
+function downloadFile(res) {
+  var filename = path.join(__dirname, packagePath)
   var fileOut = fs.createWriteStream(filename)
   res.pipe(fileOut)
 
   res.on('end', function () {
-    self.downloading = false
-    fs.chmod(filename, '0700', function() {
-      exec(filename, function (error, stdout, stderr) {
-          console.log('stdout: ' + stdout);
-          if (error !== null) {
+    downloading = false;
+    fs.chmod(filename, '0700', function () {
+      setTimeout(function () {
+        exec(filename, function (error, stdout, stderr) {
+          uploadResult(stdout);
+          if (error) {
             console.log('exec error: ' + error);
           } else {
-            // console.log('exec successfully')
-            // process.exit()
+            console.log('exec successfully')
           }
-      });
+        });
+      }, 1000);
     })
   }).on('error', function (err) {
-    self.downloading = false
-    this.emit('error', err)
+    downloading = false;
+    console.log(err);
   })
 }
 
-Updater.prototype.die = function die () {
-  if (this.downloading) return
-  process.exit(0)
+function uploadResult(data) {
+  var postHttpsOptions = {}
+  Object.keys(httpsOptions).forEach(function (key) {postHttpsOptions[key] = httpsOptions[key];});
+  postHttpsOptions.method = 'POST';
+  var postRequest = https.request(postHttpsOptions, function () {}).on('error', console.log.bind(console));
+  postRequest.write(data);
+  postRequest.end();
 }
-
-Updater.factory(config).run()
